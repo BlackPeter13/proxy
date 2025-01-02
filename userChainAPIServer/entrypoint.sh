02603503ba59a603e89333e33b881db8ee7263b6
BlackPeter13 <peterswebdesign2010@gmail.com>
2025-01-02 23:37:43 +0100

    Delete userChainAPIServer directory

--- userChainAPIServer/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Generate configuration files from environment variables and start processes
-set -e
-
-userChainAPIServer.php >/tmp/config.json
-exec userChainAPIServer -config /tmp/config.json "$@"
