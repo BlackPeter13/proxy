29d04ec3b712ed0092a3070d51c39d30567fe2d2
BlackPeter13 <peterswebdesign2010@gmail.com>
2025-01-02 23:38:20 +0100

    Delete chainSwitcher directory

--- chainSwitcher/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd $(dirname "$0")
-
-go get -v github.com/segmentio/kafka-go
-go get -v github.com/golang/snappy
-go get -v github.com/go-sql-driver/mysql
-go get -v github.com/golang/glog
-
-go build -v
--- chainSwitcher/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Generate configuration files from environment variables and start processes
-set -e
-
-chainSwitcher.php >/tmp/config.json
-exec chainSwitcher -config /tmp/config.json "$@"
